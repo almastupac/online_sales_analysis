36d2165be1c889471d93747b1162f7eae7f99a04
almastupac <almadizdar29@gmail.com>
2025-01-31 11:38:19 +0100

    Deleting code line total value and display info

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from product_manager import ProductManager
 
 #  Creating instance
 manager1 = ProductManager()
-product1 = Product('T-Shirt', 19.50, 2)
-product2 = Product('Jeans', 79.99, 3)
-product3 = Product('Jacket', 145.20, 1)
+product1 = Product('Hat', 19.50, 2)
+product2 = Product('Shoes', 79.99, 1)
+product3 = Product('Pullover', 75.00, 1)
 
 
 # Adding products
@@ -14,9 +14,3 @@ manager1.add_product(product2)
 manager1.add_product(product3)
 
 
-#  Display products list
-print(manager1.info_products())
-
-
-#  Display total value
-print(manager1.total_value())
